d46561fe00b3fb32b21689b1750395f797f2cb49
Kenneth Popay <kenneth.popay@wonderbill.com>
2020-03-04 19:51:30 +0000

    Add composeWithDevTools to allow state visivility through redux-devtools-extension

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './reducer'
 
-export default createStore(reducer)
+export default createStore(reducer, composeWithDevTools(applyMiddleware()))
