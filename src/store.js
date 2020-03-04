692d0fb9a49af54f21728f868d61c61fa348d19c
Kenneth Popay <kenneth.popay@wonderbill.com>
2020-03-04 20:00:00 +0000

    Include redux-thunk for asynchronous action dispatches

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './reducer'
 
-export default createStore(reducer, composeWithDevTools(applyMiddleware()))
+export default createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
