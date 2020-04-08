f176c7c5ef0e9c0286342f24132ea9fb775b9674
Kenneth Popay <kenneth.popay@wonderbill.com>
2020-04-08 01:00:11 +0100

    Setup the linting properly so it works with latest react features etc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,39 @@
 module.exports = {
   root: true,
-  plugins: [
-    'react',
-    'react-native',
-    'jest',
-    'prettier',
-    'eslint-plugin-react-hooks'
-  ],
-  rules: {
-    'react-hooks/rules-of-hooks': error,
-    'react-hooks/exhaustive-deps': warning,
-    'react/jsx-uses-react': error,
-    'react/jsx-uses-vars': error,
-    'linebreak-style': [error, 'unix'],
-    quotes: [warning, 'single', {avoidEscape: true}],
-    semi: [error, 'never'],
-    'prefer-arrow-callback': error,
-    'react-native/no-inline-styles': error,
-    'react-native/no-unused-styles': error,
-    'no-unused-vars': error,
-    'no-undef': error,
-    // 'no-restricted-imports': [error, { patterns: ['../*', '../'] }],
-    'no-console': [error, {allow: ['warn', 'error']}]
+  parser: 'babel-eslint',
+  plugins: ['react', 'react-native', 'react-hooks', 'jest', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: 'module'
   },
-  extends: ['plugin:prettier/recommended', '@react-native-community']
+  rules: {
+    'react/jsx-uses-react': 2,
+    'react/jsx-uses-vars': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-unused-styles': 2,
+    'react-native/no-raw-text': 2,
+    'react-native/no-single-element-style-arrays': 2,
+    'linebreak-style': [2, 'unix'],
+    quotes: [1, 'single', { avoidEscape: true }],
+    semi: [2, 'never'],
+    'no-unused-vars': 2,
+    'no-undef': 2,
+    'no-console': [2, { allow: ['warn', 'error'] }],
+    'prefer-arrow-callback': 2
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    '@react-native-community',
+    'plugin:prettier/recommended',
+    'prettier/react'
+  ],
+  env: {
+    es6: true,
+    node: true,
+    jest: true
+  }
 }
