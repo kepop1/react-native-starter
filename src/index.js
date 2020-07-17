f5e6f1de8ea301e38d19f7710cb84142c894eb79
kepop1 <kepop123@gmail.com>
2020-07-17 18:22:34 +0100

    Adjust android folder for future packaging, tweaked package json and added some nice to haves as default text scaling and safe area in the view

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import 'react-native-gesture-handler' //Required for React-Navigation >v5 to be at the top
 import React from 'react'
+import { Text, StatusBar } from 'react-native'
 import { Provider } from 'react-redux'
 import { NavigationContainer } from '@react-navigation/native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import store from './store'
 import RootNavigator from './navigation/RootNavigator'
+
+//This disables any of the Text from scaling according to the OS settings for text.
+//This is not a good idea long term and should be addressed!
+Text.defaultProps = {
+  ...(Text.defaultProps || {}),
+  allowFontScaling: false
+}
 
 export default () => (
   <Provider store={store}>
     <NavigationContainer>
-      <RootNavigator />
+      <SafeAreaProvider>
+        <StatusBar barStyle="light-content" />
+        <RootNavigator />
+      </SafeAreaProvider>
     </NavigationContainer>
   </Provider>
 )
